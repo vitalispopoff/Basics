8a617e0924b7f64b6f83379950d7f5c4798519c9
vitalispopoff <vitalis.popoff@gmail.com>
2021-08-07 17:35:30 +0200

    refactorization - ugly ?

--- Basics/code/ch19.cpp
+++ Basics/code/ch19.cpp
@@ -6,85 +6,83 @@ using namespace std;
 
 namespace ch19
 {
-	template <typename T, typename A>
-		m_vector<T, A> & m_vector<T, A>::operator = (const m_vector & v)
-	{
-		if (this == & v)
-			return * this;
-		if (space < v.sz)
-		{
-			delete [] elem;
-			elem = alloc.allocate (v.sz);
-			space = v.sz;
-		}
-		sz = v.sz;			
-		copy (v.elem, v.elem + sz, elem);
-		return * this;
-	}
+	//template <typename T, typename A>
+	//	m_vector<T, A> & m_vector<T, A>::operator = (const m_vector & v)
+	//{
+	//	if (this == & v)
+	//		return * this;
+	//	if (space < v.sz)
+	//	{
+	//		delete [] elem;
+	//		elem = alloc.allocate (v.sz);
+	//		space = v.sz;
+	//	}
+	//	sz = v.sz;			
+	//	copy (v.elem, v.elem + sz, elem);
+	//	return * this;
+	//}
 	
-	template <typename T, typename A>
-		m_vector<T, A> & m_vector<T, A>::operator = (m_vector && v) noexcept (true)
-	{
-		if (true)
-			delete [] elem;
-		else
-		{
-			alloc.destroy (space, elem);
-			alloc.deallocate (elem);
-		}
+	//template <typename T, typename A>
+	//	m_vector<T, A> & m_vector<T, A>::operator = (m_vector && v) noexcept (true)
+	//{
+	//	if (true)
+	//		delete [] elem;
+	//	else
+	//	{
+	//		alloc.destroy (space, elem);
+	//		alloc.deallocate (elem);
+	//	}
+	//	sz = v.sz;
+	//	space = v.space;
+	//	elem = v.elem;
+	//	v.elem = nullptr;
+	//	v.sz = v.space = 0;
+	//	return * this;
+	//}
+	
+	//template <typename T, typename A>
+	//	void m_vector<T, A>::reserve (int new_space)
+	//{
+	//	if (new_space <= space)
+	//		return;
+	//	T * p = alloc.allocate(new_space);
+	//	for (int i = 0; i < sz; ++i)
+	//	{
+	//		alloc.construct(& p [i], elem [i]);
+	//		alloc.destroy (& elem[i]);
+	//	}
+	//	alloc.deallocate (elem, space);
+	//	elem = p;
+	//	space = new_space;
+	//}
+	
+	//template <typename T, typename A>
+	//	void m_vector<T, A>::resize (int new_size, T val)
+	//{		
+	//	if (new_size < 0)
+	//		return;
 
-		sz = v.sz;
-		space = v.space;
-		elem = v.elem;
-		v.elem = nullptr;
-		v.sz = v.space = 0;
-		return * this;
-	}
+	//	reserve (new_size);
+	//		for (int i = sz; i < new_size; ++i)
+	//			alloc.construct (&elem [i], val);
+	//		for (int i = new_size; i < sz; ++i)
+	//			alloc.destroy (&elem [i]);
+	//		sz = new_size;
+	//}
 	
-	template <typename T, typename A>
-		void m_vector<T, A>::reserve (int new_space)
-	{
-		if (new_space <= space)
-			return;
-		T * p = alloc.allocate(new_space);
-
-		for (int i = 0; i < sz; ++i)
-		{
-			alloc.construct(& p [i], elem [i]);
-			alloc.destroy (& elem[i]);
-		}
-		alloc.deallocate (elem, space);
-		elem = p;
-		space = new_space;
-	}
-	
-	template <typename T, typename A>
-		void m_vector<T, A>::resize (int new_size, T val)
-	{		
-		if (new_size < 0)
-			return;
-
-		reserve (new_size);
-			for (int i = sz; i < new_size; ++i)
-				alloc.construct (&elem [i], val);
-			for (int i = new_size; i < sz; ++i)
-				alloc.destroy (&elem [i]);
-			sz = new_size;
-	}
-	
-	template <typename T, typename A>
-		void m_vector<T, A>::push_back (T val)
-	{
-		if (space == 0)
-			reserve (8);		
-		/// is 'else' redundant?: if space==0 reserve(8) returns sz==0 && space==8
-		/// but if space!=0 then and only then space?=sz is to be checked
-		else
-			if (space == sz)
-				reserve (space * 2);
-		alloc.construct (& elem [sz], val);
-		++sz;
-	}
+	//template <typename T, typename A>
+	//	void m_vector<T, A>::push_back (T val)
+	//{
+	//	if (space == 0)
+	//		reserve (8);		
+	//	/// is 'else' redundant?: if space==0 reserve(8) returns sz==0 && space==8
+	//	/// but if space!=0 then and only then space?=sz is to be checked
+	//	else
+	//		if (space == sz)
+	//			reserve (space * 2);
+	//	alloc.construct (& elem [sz], val);
+	//	++sz;
+	//}
 
 //	----------------------------------------------------------
 	
@@ -99,134 +97,137 @@ namespace ch19
 
 	void test_01()
 	{
+		int test_no {0};
 		if (true)
 		{
 			m_vector<int> v;
 			testing ("empty constr: size", v.size(), 0);
 			testing ("empty constr: space", v.capacity(), 8);
+			++test_no;
 		}
+		
 		if (true)
 		{
 			m_vector<int> v (1, -1);
 			testing ("expl constr: size", v.size(), 1);
 			testing ("expl constr: space", v.capacity(), 1);
 			testing ("expl constr: elem", v[0], -1);
+			++test_no;
 		}
-		if (true)
-		{
-			m_vector<int> v {-1};
-			testing ("init constr: size", v.size(), 1);
-			testing ("init constr: space", v.capacity(), 1);
-			testing ("init constr: elem", v[0], -1);
-		}
-		if (true)
-		{
-			m_vector<int> 
-				u {-1},
-				v {u};
-			testing ("copy constr: size", v.size(), 1);
-			testing ("copy constr: space", v.capacity(), 1);
-			testing ("copy constr: elem", v[0], -1);
-		}
-		if (true)
-		{	
-			auto a = []() -> m_vector<int>
-			{
-				m_vector<int> temp {-1};
-				return temp;
-				 //return m_vector<int>(); //doesn't call the copy/move constr !!!
-			};
-			m_vector<int> v {a()};
-			
-			testing ("move constr: size", v.size(), 1);
-			testing ("move constr: space", v.capacity(), 1);
-			testing ("move constr: elem", v[0], -1);
-		}
-		if (true)
-		{
-			m_vector<int>
-				v = m_vector<int> {-1};
-			testing ("= oper: size", v.size(), 1);
-			testing ("= oper: space", v.capacity(), 1);
-			testing ("= oper: elem", v[0], -1);
-		}
-		if (true)
-		{
-			m_vector<int> 
-				u {-1},
-				v = u;
-			testing ("= copy oper: size", u.size(), v.size());
-			testing ("= copy oper: space", u.capacity(), v.capacity());
-			testing ("= copy oper: elem", u[0], v[0]);
-		}
-		if (true)
-		{
-			void 
-				* ptr = nullptr;
-			auto a = [&]() -> m_vector<int>
-			{
-				m_vector<int> temp {-1};
-				ptr = (void *)temp.addr();
-				return temp;
-			};
-			m_vector<int>
-				v = a();
-			testing ("= move oper: size", v.size(), 1);
-			testing ("= move oper: space", v.capacity(), 1);
-			testing ("= move oper: elem", v[0], -1);			
-			testing ("= move oper: *", (int) ptr, (int) v.addr());
-		}
-		if (true)
-		{
-			m_vector<int>
-				v(0);
-			v.reserve (4);
-			testing ("reserve: size", v.size(), 0);
-			testing ("reserve: space", v.capacity(), 4);
-		}
-		if (true)
-		{
-			m_vector<int>
-				v {-1};
-			v.resize(2);
-
-			testing ("resize: size", v.size(), 2);
-			testing ("resize: space", v.capacity(), 2);
-			testing ("resize: elem", v[0], -1);
-		}
-		if (false)
-		{
-			auto skrt = [](const auto & v)	// 'auto' XD
-			{
-				for (int i = 0; i < v.size(); ++i)
-					cout
-						<< v[i] << ", ";
-				cout << '\n';
-			};
-
-			m_vector <double>
-				a {0, 1, 2, 3},
-				b;
-
-			skrt(a);
 		
-			a.push_back(-1);
+		//if (false)
+		//{
+		//	m_vector<int> v {-1};
+		//	testing ("init constr: size", v.size(), 1);
+		//	testing ("init constr: space", v.capacity(), 1);
+		//	testing ("init constr: elem", v[0], -1);
+		//}
+		
+		//if (false)
+		//{
+		//	m_vector<int> 
+		//		u {-1},
+		//		v {u};
+		//	testing ("copy constr: size", v.size(), 1);
+		//	testing ("copy constr: space", v.capacity(), 1);
+		//	testing ("copy constr: elem", v[0], -1);
+		//}
+		
+		//if (false)
+		//{	
+		//	auto a = []() -> m_vector<int>
+		//	{
+		//		m_vector<int> temp {-1};
+		//		return temp;
+		//		 //return m_vector<int>(); //doesn't call the copy/move constr !!!
+		//	};
+		//	m_vector<int> v {a()};
+		//	
+		//	testing ("move constr: size", v.size(), 1);
+		//	testing ("move constr: space", v.capacity(), 1);
+		//	testing ("move constr: elem", v[0], -1);
+		//}
+		
+		//if (false)
+		//{
+		//	m_vector<int>
+		//		v = m_vector<int> {-1};
+		//	testing ("= oper: size", v.size(), 1);
+		//	testing ("= oper: space", v.capacity(), 1);
+		//	testing ("= oper: elem", v[0], -1);
+		//}
+		
+		//if (false)
+		//{
+		//	m_vector<int> 
+		//		u {-1},
+		//		v = u;
+		//	testing ("= copy oper: size", u.size(), v.size());
+		//	testing ("= copy oper: space", u.capacity(), v.capacity());
+		//	testing ("= copy oper: elem", u[0], v[0]);
+		//}
+		
+		//if (false)
+		//{
+		//	void 
+		//		* ptr = nullptr;
+		//	auto a = [&]() -> m_vector<int>
+		//	{
+		//		m_vector<int> temp {-1};
+		//		ptr = (void *)temp.addr();
+		//		return temp;
+		//	};
+		//	m_vector<int>
+		//		v = a();
+		//	testing ("= move oper: size", v.size(), 1);
+		//	testing ("= move oper: space", v.capacity(), 1);
+		//	testing ("= move oper: elem", v[0], -1);			
+		//	testing ("= move oper: *", (int) ptr, (int) v.addr());
+		//}
+		
+		//if (false)
+		//{
+		//	m_vector<int>
+		//		v(0);
+		//	v.reserve (4);
+		//	testing ("reserve: size", v.size(), 0);
+		//	testing ("reserve: space", v.capacity(), 4);
+		//}
+		
+		//if (false)
+		//{
+		//	m_vector<int>
+		//		v {-1};
+		//	v.resize(2);
+		//	testing ("resize: size", v.size(), 2);
+		//	testing ("resize: space", v.capacity(), 2);
+		//	testing ("resize: elem", v[0], -1);
+		//}
 
-			skrt(a);
-
-			a.resize (b.capacity());
-
-			a.reserve (16);
-
-			skrt(a);
-	
-			a.resize (-1);
-
-			a.resize (4);
-
-			skrt (a);
-		}
-		cout << "all tests done\n";
+		//if (false)
+		//{
+		//	auto skrt = [](const auto & v)	// 'auto' XD
+		//	{
+		//		for (int i = 0; i < v.size(); ++i)
+		//			cout
+		//				<< v[i] << ", ";
+		//		cout << '\n';
+		//	};
+		//	m_vector <double>
+		//		a {0, 1, 2, 3},
+		//		b;
+		//	skrt(a);
+		//
+		//	a.push_back(-1);
+		//	skrt(a);
+		//	a.resize (b.capacity());
+		//	a.reserve (16);
+		//	skrt(a);
+		//	a.resize (-1);
+		//	a.resize (4);
+		//	skrt (a);
+		//}
+		cout << "all tests done (" << test_no << ")\n";
 	}
 
 	void main()
--- Basics/code/ch19.h
+++ Basics/code/ch19.h
@@ -17,6 +17,13 @@ namespace ch19
 		int sz;
 		int space;
 
+		vector_base () :
+			alloc {A ()},
+			elem {alloc.allocate (8)},
+			sz {0},
+			space {8}
+		{}
+
 		vector_base (const A & a, int n) :
 			alloc {a},
 			elem {alloc.allocate (n)},
@@ -27,76 +34,83 @@ namespace ch19
 	};
 		
 	template <typename T, typename A = allocator <T>>	// T is Element <E>()
-	class m_vector
+	class m_vector : private vector_base<T, A>
 	{
-		A alloc;
-		int sz;
-		int space;
-		T * elem;
+		//A alloc;
+		//int sz;
+		//int space;
+		//T * elem;
 
 	public : 
 
-		m_vector () :
-			sz {0},
-			space {8},
-			elem {alloc.allocate (space)}
-		{}
-
-		explicit m_vector (int n, T val = T()) :
-			sz {max (0, n)},
-			space {max(1, n)},
-			elem {alloc.allocate (space)}
+		m_vector ()
+		//	sz {0},
+		//	space {8},
+		//	elem {alloc.allocate (space)}
 		{
-			for (int i = 0; i < sz; ++i) 
-				alloc.construct (& elem [i], val);
+			this -> alloc = A ();
+			this -> sz = 0;
+			this -> space = 8;
+			this -> elem = this -> alloc.allocate (this -> space);
+		
 		}
 
-		m_vector (initializer_list <T> lst) :
-			sz {(int) lst.size()},
-			space {(int) lst.size()},
-			elem {alloc.allocate (space)}
+		explicit m_vector (int n, T val = T())
 		{
-			copy (lst.begin(), lst.end(), elem);
+			this -> sz = max (0, n);
+			this -> space = max(1, n);
+			this -> elem = this -> alloc.allocate (this -> space);
+
+			for (int i = 0; i < this -> sz; ++i) 
+				this->alloc.construct (& (this->elem [i]), val);
 		}
 
-		m_vector (const m_vector & v) :
-			sz {v.sz},
-			space {sz},
-			elem {new T [sz]}
-		{
-			copy (v.elem, v.elem + sz, elem);
-		}
+		//m_vector (initializer_list <T> lst) :
+		//	sz {(int) lst.size()},
+		//	space {(int) lst.size()},
+		//	elem {alloc.allocate (space)}
+		//{
+		//	copy (lst.begin(), lst.end(), elem);
+		//}
 
-		m_vector (m_vector && v) noexcept (true) :
-			sz {v.sz},
-			space {v.space},
-			elem {v.elem}
-		{
-			v.sz = v.space = 0;
-			v.elem = nullptr;
-		}
+		//m_vector (const m_vector & v) :
+		//	sz {v.sz},
+		//	space {sz},
+		//	elem {new T [sz]}
+		//{
+		//	copy (v.elem, v.elem + sz, elem);
+		//}
+
+		//m_vector (m_vector && v) noexcept (true) :
+		//	sz {v.sz},
+		//	space {v.space},
+		//	elem {v.elem}
+		//{
+		//	v.sz = v.space = 0;
+		//	v.elem = nullptr;
+		//}
 
 		~m_vector ()
 		{
-			delete [] elem;		// keep delete or connect with alloc ?
+			//delete [] elem;		// keep delete or connect with alloc ?
 		}
 
-		m_vector & operator = (const m_vector & v);
-		m_vector & operator = (m_vector && v) noexcept (true);
+		//m_vector & operator = (const m_vector & v);
+		//m_vector & operator = (m_vector && v) noexcept (true);
 		
 
-		T & operator [] (int n) {return elem [n];}
-		const T & operator [] (int n) const {return elem [n];}
+		T & operator [] (int n) {return this -> elem [n];}
+		const T & operator [] (int n) const {return this -> elem [n];}
 
-		void reserve (int new_space);
-		void resize(int new_size, T val = T());
-		void push_back (T val);
+		//void reserve (int new_space);
+		//void resize(int new_size, T val = T());
+		//void push_back (T val);
 
-		int size () const {return sz;}
-		int capacity () const {return space;}
+		int size () const {return this -> sz;}
+		int capacity () const {return this -> space;}
 
 		/// for testing:
-		const T * addr () const {return elem;}
+		const T * addr () const {return this -> elem;}
 	};
 
 
