47e855fef3f070e681a837d866363b148eb2ba85
vitalispopoff <vitalis.popoff@gmail.com>
2021-08-09 19:38:58 +0200

    dril 3, 4

--- Basics/code/ch19_drill.cpp
+++ Basics/code/ch19_drill.cpp
@@ -1,10 +1,26 @@
 #include "ch19.h"
 
+#include <vector>
+
 namespace ch19_drill
 {
+	template <typename T> void show (S<T> in)
+	{
+		cout << in.val << '\n';
+	}
 	void main()
 	{
-		S<int> s (1);
-		cout << s.val;
+		S <int> i {-1};
+		show (i);
+		S <char> c {'@'};
+		show (c);
+		S <double> d {1.618};
+		show (d);
+		S <string> s {""};
+		show (s);
+		S <vector<int>> v {{1,2,3}};
+		for (int x : v.val)
+			cout 
+				<< x << ", ";
 	}
 }
