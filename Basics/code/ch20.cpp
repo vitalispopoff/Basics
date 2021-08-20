bdbaf1de67121c4cce12e9ef28311cd8dd2427d7
Vitalis Popoff <vitalis.popoff@gmail.com>
2021-08-20 18:03:09 +0200

    ch20 try this 5 - ugly implementation

--- Basics/code/ch20.cpp
+++ Basics/code/ch20.cpp
@@ -5,12 +5,10 @@
 
 namespace ch20
 {
-	//using namespace std;
 	using namespace testing_space;
 
 	namespace try_this_1
 	{
-		/// these two implemented to avoid building errors only
 		double * get_from_jack (int * count)
 		{
 			* count = 3;			
@@ -108,10 +106,375 @@ namespace ch20
 			report (no, name);
 		}
 	}
+
+	namespace try_this_4
+	{
+		bool flag {false};
+		double * get_from_jack (int * count, bool b = flag)
+		{
+			if (b)
+				* count = 3;			
+				return new double [3] { -1, -1, -1};
+			return nullptr;
+		}
+		vector <double> * get_from_jill(bool b = flag)
+		{			
+			return b ? 
+				new vector <double> {-1, -1, -1}
+				: nullptr;
+		}
+
+		template <typename Iterator>
+		Iterator high (Iterator first, Iterator last)
+		{
+			if (first == last)
+				throw runtime_error ("No data available");
+
+			Iterator 
+				high = first;
+			for (Iterator p = first; p != last; ++p)
+				if (* high < * p) 
+					high = p;
+			return high;
+		}
+
+		void fct()
+		{
+			int
+				jack_count = 0;
+			double
+				* jack_data = get_from_jack (& jack_count);
+			vector <double> 
+				* jill_data = get_from_jill ();
+			double
+				* jack_high = high (jack_data, jack_data + jack_count);
+			vector <double>
+				& v = * jill_data;
+			double 
+				* jill_high = high (& v [0], & v [0] + v.size());
+
+			delete [] jack_data;
+			delete jill_data;
+		}
+
+		void test()
+		{
+			string
+				name {"ch20: try this 4"};
+			int 
+				no = test_no;
+			try
+			{
+				fct();
+			}
+			catch (exception & e)
+			{
+				testing_bundle <string>
+					t_err {name, e.what(), "No data available"};
+			}
+			report (no, name);
+		}
+
+	}
+
+	namespace try_this_5
+	{
+		template <typename T, typename A>
+		m_vector <T, A> & m_vector <T, A>::operator = (const m_vector & v)
+		{
+			if (this == & v)
+				return * this;
+			if (this -> space < v.sz)
+			{
+				this -> elem.reset (this -> alloc.allocate (v.sz));
+				this -> space = v.sz;
+			}
+			this -> sz = v.sz;
+			copy (v.elem.get(), v.elem.get() + this -> sz, this -> elem.get());
+			return * this;
+		}
+
+		template <typename T, typename A>
+		m_vector <T, A> & m_vector <T, A>::operator = (m_vector && v) noexcept
+		{
+			this -> sz = v.sz;
+			this -> space = v.space;
+			this -> elem.reset(v.elem.release());
+			return * this;
+		}
+
+		template <typename T, typename A>
+		void m_vector <T, A>::reserve (int new_space)
+		{
+			if (new_space <= this -> space)
+				return;
+			unique_ptr <T> 
+				ptr {this -> alloc.allocate (new_space)};
+			copy (this -> elem.get(), this -> elem.get() + this -> sz, ptr.get());
+			this -> elem.reset (ptr.release());		
+			this -> space = new_space;
+		}
+
+		template <typename T, typename A>
+		void m_vector <T, A>::resize (int new_size, T val)
+		{
+			if (new_size < 0)
+				return;
+			reserve (new_size);
+			for (int i = this -> sz; i < new_size; ++i)
+				this -> alloc.construct (& this -> elem.get() [i], val);
+			for (int i = new_size; i < this -> sz; ++i)
+				this -> alloc.destroy (& this -> elem.get() [i]);
+			this -> sz = new_size;
+		}
+
+		template <typename T, typename A>
+		void m_vector <T, A>::push_back (T val)
+		{
+			if (this -> space == 0)
+				reserve (8);
+			else 
+				if (this -> space == this -> sz)
+					reserve (2 * this -> space);
+			this -> alloc.construct (& this -> elem.get() [this -> sz], val);
+			++this -> sz;
+		}
+
+		template <typename T, typename A>
+		void m_vector <T, A>::push_front (T val)
+		{
+			int
+				new_space {this -> sz < this -> space ? this -> space : this -> space * 2};
+			unique_ptr <T>
+				ptr {this -> alloc.allocate (new_space)};
+			* ptr.get() = val;
+			copy (this -> elem.get(), this -> elem.get() + this -> sz, ptr.get() + 1);
+			this -> elem.reset (ptr.release());
+			this -> space = new_space;
+			++ this-> sz;
+		}
+
+		template <typename T>
+		void printing (m_vector <T> & v)
+		{
+			cout
+				<< "\nsz : " << v.size() <<",\tspace : " << v.capacity() << '\n';
+		}
+
+		void test_min()
+		{
+			string
+				name {"ch20: try this 5"};
+			int 
+				no = test_no;
+				
+			m_vector <int> 
+				v0;
+			testing_bundle <int>
+				t0_0 {name + ": empty size", v0.size(), 0},
+				t0_1 {name + ": empty space", v0.capacity(), 8};
+			report (no, name + ": empty");
+
+			m_vector <int> 
+				v1 (1, -1);
+			testing_bundle <int>
+				t1_0 {name + ": expl size", v1.size(), 1},
+				t1_1 {name + ": expl space", v1.capacity(), 1},
+				t1_2 {name + ": expl elem", v1[0], -1};
+			report (no, name + ": expl");
+
+			m_vector <int> v2 {-1, -2};
+			testing_bundle <int>
+				t2_0 {name + ": init size", v2.size(), 2},
+				t2_1 {name + ": init space", v2.capacity(), 2},
+				t2_2 {name + ": init elem", v2 [0], -1},
+				t2_3 {name + ": init elem", v2 [1], -2};
+			report (no, name + ": init");
+
+			m_vector <int> 
+				u3 {-1},
+				v3 {u3};
+			testing_bundle <int>
+				t3_0 {name + ": copy size", v3.size(), 1},
+				t3_1 {name + ": copy space", v3.capacity(), 1},
+				t3_2 {name + ": copy elem", v3 [0], -1};
+			report (no, name + ": copy");
+
+			void 
+				* temp_ptr4 = nullptr;
+			auto a4 = [&]() -> m_vector <int>
+			{
+				m_vector <int> 
+					temp {-1};
+				temp_ptr4 = (void *) temp.addr();
+				return temp;
+			};
+			m_vector <int> 
+				v4 {a4()};
+			testing_bundle <int>
+				t4_0 {name + ": move size", v4.size(), 1},
+				t4_1 {name + ": move space", v4.capacity(), 1},
+				t4_2 {name + ": move *", (int) temp_ptr4, (int) v4.addr()};
+			report (no, name + ": move");
+			
+			m_vector <int>
+				v5 = m_vector <int> {-1};
+			testing_bundle <int>
+				t5_0 {name + ": = size", v5.size(), 1},
+				t5_1 {name + ": = space", v5.capacity(), 1},
+				t5_2 {name + ": = elem", v5[0], -1};
+			report (no, name + ": =");
 	
+			m_vector <int>
+				u6 {-1},
+				v6 = u6;
+			testing_bundle <int>
+				t6_0 {name + ": = copy size", u6.size(), v6.size()},
+				t6_1 {name + ": = copy space", u6.capacity(), v6.capacity()},
+				t6_2 {name + ": = copy elem", u6 [0], v6 [0]};
+			report (no, name + ": = copy");
+
+			void 
+				* ptr7 = nullptr;
+			auto a7 = [&]() -> m_vector <int>
+			{
+				m_vector <int> temp {-1};
+				ptr7 = (void *) temp.addr();
+				return temp;
+			};
+			m_vector <int>
+				v7 = a7();
+			testing_bundle <int>
+				t7_0 {name + ": = move size", v7.size(), 1},
+				t7_1 {name + ": = move space", v7.capacity(), 1},
+				t7_2 {name + ": = move: *", (int) ptr7, (int) v7.addr()};
+			report (no, name + "= move");
+		}
+
+		void test_reserve()
+		{
+			string
+				name {"reserve"};
+			int
+				no = test_no;
+			m_vector <int> 
+				v;
+			v.reserve (1);
+			testing_bundle <int>
+				t8_0 {name + ": empty, 1: size", v.size(), 0},
+				t8_1 {name + ": empty, 1: space", v.capacity(), 8},
+				t8_2 {name + ": empty, 1: *", int (v.addr() != nullptr), 1};
+			report (no, name);
+		}
+
+		void test_resize()
+		{
+			string 
+				name {"resize"};
+			int
+				no = test_no;
+			m_vector <int> 
+				v1;
+			v1.resize(0);
+			testing_bundle <int>
+				t9_0 {name + ": init, 0 : size", v1.size(), 0},
+				t9_1 {name + ": init, 0 : space", v1.capacity(), 8},
+				t9_2 {name + ": init, 0 : *", int (v1.addr() != nullptr), 1};
+			report (no, name);
+
+			m_vector <int> 
+				vA;
+				vA.resize(1);
+			testing_bundle <int>
+				tA_0 {name + ": init, 1 : size", vA.size(), 1},
+				tA_1 {name + ": init, 1 : space", vA.capacity(), 8},
+				tA_2 {name + ": init, 1 : elem", vA [0], 0};
+			report (no, name);
+
+			m_vector <int> 
+				vB;
+			vB.resize(1, -1);
+			testing_bundle <int>
+				tB_0 {name + ": init, 1 : size", vB.size(), 1},
+				tB_1 {name + ": init, 1 : space", vB.capacity(), 8},
+				tB_2 {name + ": init, 1 : elem", vB [0], -1};
+			report (no, name);
+
+			m_vector <int> vC {-1, -2};
+			vC.resize (1);
+			testing_bundle <int>
+				tC_0 {name + ": init 2, 1 : size", vC.size(), 1},
+				tC_1 {name + ": init 2, 1 : space", vC.capacity(), 2},
+				tC_2 {name + ": init 2, 1 : elem", vC [0], -1};
+			report (no, name);
+		}
+
+		void test_push_back()
+		{
+			string
+				name {"push_back"};
+			int
+				no = test_no;
+
+			m_vector <int> 
+				vD;
+			vD.push_back (-1);
+			testing_bundle <int>
+				tD_0 {name + ": empty : size", vD.size(), 1},
+				tD_1 {name + ": empty : space", vD.capacity(), 8},
+				tD_2 {name + ": empty : elem", vD[0], -1};
+			report (no, name);
+
+			m_vector <int> 
+				vE (8);
+			void 
+				* ptrE = (void *) vE.addr();
+			vE.push_back (-1);
+			testing_bundle <int>
+				tE_0 {name + ": expl, 8 : size", vE.size(), 9},
+				tE_1 {name + ": expl, 8 : space", vE.capacity(), 16},
+				tE_2 {name + ": expl, 8 : elem", vE [8], -1},
+				tE_3 {name + ": expl, 8 : *", int (ptrE !=  (void *) vE.addr()), 1};
+			report (no, name);
+		}
+
+		void test_push_front()
+		{
+			string
+				name {"push_front"};
+			int
+				no = test_no;
+			
+			m_vector <int>
+				vF;
+			vF.push_front (-1);
+			testing_bundle <int>
+				tF_0 {name + ": empty : size", vF.size(), 1},
+				tF_1 {name + ": empty : space", vF.capacity(), 8},
+				tF_2 {name + ": empty : elem", vF [0], -1};
+
+			report (no, name);
+		}
+
+		void tests()
+		{
+			test_min();
+			test_reserve();
+			test_resize();
+			test_push_back();
+			test_push_front();
+
+			cout << "\nAll tests done (" << test_no << ").\n";		
+		}
+
+		void main()
+		{
+			tests();
+		}		
+	}
 
 	void main()
 	{
-		try_this_3::test();
+		try_this_5::tests();
 	}
 }
--- Basics/code/ch20.h
+++ Basics/code/ch20.h
@@ -20,5 +20,82 @@ namespace ch20
 		void test();
 	}
 
+	namespace try_this_4
+	{
+		void test();
+	}
+
+	/// copy from ch19_go_deeper
+	namespace try_this_5
+	{
+		template <typename T, typename A = allocator <T>>
+		struct vector_base
+		{
+			A alloc;
+			int sz, space;
+			unique_ptr<T> elem;
+			explicit vector_base (const A & a = A (), int n = 0) :
+				alloc {a},
+				sz {n},
+				space {n > 0 ? n : 8},
+				elem {alloc.allocate (space)}
+			{}
+			~vector_base () {elem.reset();}
+		};
+
+		template <typename T, typename A = allocator <T>>
+		class m_vector : private vector_base <T, A>
+		{
+		public :
+			m_vector () :
+				vector_base <T, A>::vector_base{}
+			{}
+
+			explicit m_vector (int n, T val = T()) :
+				vector_base <T,A>::vector_base {A (), max (0, n)}
+			{
+				for (int i = 0; i < this -> sz; ++i)
+					this->alloc.construct (& this -> elem.get() [i], val);
+			}
+
+			m_vector (initializer_list <T> lst) :
+				vector_base <T, A>::vector_base {A (), (int) lst.size()} 
+			{
+				copy (lst.begin(), lst.end(), this -> elem.get());
+			}
+
+			m_vector (const m_vector & v) :
+				vector_base <T, A>::vector_base {A (), v.sz}
+			{
+				copy (v.elem.get(), v.elem.get() + v.sz, this -> elem.get());
+			}
+
+			m_vector (m_vector && v) noexcept :
+				vector_base <T, A>::vector_base {}
+			{
+				this -> sz = this -> space = v.sz;			
+				this -> elem.reset (v.elem.release());
+			}
+
+			m_vector & operator = (const m_vector & v);
+			m_vector & operator = (m_vector && v) noexcept;
+
+			T & operator [] (int n) {return this -> elem.get() [n];}
+			const T & operator [] (int n) const {return this -> elem.get() [n];}
+
+			void reserve (int new_space);
+			void resize (int new_size, T val = T());
+			void push_back (T val);
+			void push_front (T val);
+
+			int size() const {return this -> sz;}
+			int capacity() const {return this -> space;}
+
+			const T * addr () const {return & this -> elem.get()[0];} // this is curious
+			unique_ptr<T> & get() {return this -> elem;}
+		};		
+
+	}
+
 	void main();
 }
