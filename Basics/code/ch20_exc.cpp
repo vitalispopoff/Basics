0ae4aa4dc76f1715b609f44d71496f0e9e8e887b
Vitalis Popoff <vitalis.popoff@gmail.com>
2021-08-23 17:08:48 +0200

    ch20 e02 init

--- Basics/code/ch20_exc.cpp
+++ Basics/code/ch20_exc.cpp
@@ -1,55 +1,64 @@
 #include "ch20_exc.h"
 
+#include <iostream>
+
 namespace ch20_exc
 {
 	namespace e02
 	{
 		double * get_from_jack (int * count)
 		{
-			return nullptr;
+			double 
+				* ptr = new double [6] {1, 3, 2, 4, 5, 0};
+			//* count = sizeof (ptr) / sizeof (ptr [0]); // wrn C6384
+			* count = 6;
+			return ptr ;
 		}
 
 		vector <double> * get_from_jill()
 		{
-			return nullptr;
+			vector <double> 
+				* ptr = new vector <double> {1, 3, 2, 5, 4, 0};
+			return ptr;
+		}
+
+		template <typename Iter>
+		Iter high (Iter first, Iter last)
+		{
+			double
+				h {- DBL_MAX};
+			Iter
+				high {};			
+			for (Iter p = first; p != last; ++p)
+				if (h < *p)
+				{
+					high = p;
+					h = * p;
+				}
+			return high;
 		}
 
 		void fct()
 		{
 			int
 				jack_count {0};
-			double 
-				* jack_data {get_from_jack(& jack_count)};
-			vector <double> 
-				* jill_data {get_from_jill()};
+			double
+				* jack_data {get_from_jack(& jack_count)},
+				* jack_high = high (jack_data, jack_data + jack_count);
 
-			double 
-				h {- DBL_MAX},
-				* jack_high = nullptr,
-				* jill_high = nullptr;
-			for (int i = 0; i < jack_count; ++i)
-				if (h < jack_data [i])
-				{
-					jack_high = & jack_data [i];
-					h = jack_data [i];
-				}
-			h = - DBL_MAX;
-			for (int i = 0; i < jill_data -> size(); ++i)
-				if (h < (* jill_data) [i])
-				{
-					jill_high = & (* jill_data) [i];
-					h = (* jill_data) [i];
-
-				}
-
+			cout << '\n' << * jack_high;
 			delete [] jack_data;
-			delete jill_data;
+
+			vector <double>
+				& jill_data = * get_from_jill();
+			auto
+				jill_high = high (jill_data.begin(), jill_data.end());
+			cout << '\n' << * jill_high;
 		}
 	}
 
 	void main()
 	{
-
-
+		e02::fct();
 	}
 }
