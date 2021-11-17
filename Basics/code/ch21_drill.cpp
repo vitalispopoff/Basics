1a9ec7f79d4cedec40a78d5318163918d5153307
Vitalis Popoff <vitalis.popoff@gmail.com>
2021-11-17 17:11:58 +0100

    ch21_drill_01 refactor

--- Basics/code/ch21_drill.cpp
+++ Basics/code/ch21_drill.cpp
@@ -39,12 +39,6 @@ namespace ch21
 
 				Item (string n, int i, double v) : name {n}, iid {i}, value {v} {}
 
-				//Item (const Item & item) :
-				//	name {item.name},
-				//	iid {item.iid},
-				//	value {item.value}
-				//{}
-
 				string to_string()
 				{					
 					return name + " " + num_to_string (iid) + " " + num_to_string (value);
@@ -61,52 +55,73 @@ namespace ch21
 				return is  >> item.name  >> item.iid  >> item.value;
 			}
 
-			void local ()
+			string initial (bool flag)
 			{
-				vector <Item>
-					source {
-						{"Marzena", 0, 1.234},
-						{"Bo¿ena", 1, 2.52}, 
-						{"Gra¿yna", 2, 6.3}, 
-						{"Halyna", 3, 0.352},
-						{"Jarzyna", 4, 952.4}, 
-						{"Rysiek", 5, -12.5},
-						{"Zbysiek", 6, 0.0},
-						{"Czesiek", 7, 6.01},
-						{"Wiesiek", 8, 2.59},
-						{"£ysy", 9, 421.0},
-					};
-
 				string
 					filename {"..\\Basics\\resources\\ch21_d01.txt"};
 
-				ofstream
-					ofs {filename};
-				ostream_iterator <Item>
-					oi {ofs, " "};
-				copy (source.begin(), source.end(), oi);
+				if (flag)
+				{
+					vector <Item>
+						source {
+							{"Marzena", 0, 1.234},
+							{"Bo¿ena", 1, 2.52}, 
+							{"Gra¿yna", 2, 6.3}, 
+							{"Halyna", 3, 0.352},
+							{"Jarzyna", 4, 952.4}, 
+							{"Rysiek", 5, -12.5},
+							{"Zbysiek", 6, 0.0},
+							{"Czesiek", 7, 6.01},
+							{"Wiesiek", 8, 2.59},
+							{"£ysy", 9, 421.0},
+					};
 
-				ofs.close();	// shouldn't be like that?
+					ofstream
+						ofs {filename};
+					ostream_iterator <Item>
+						oi {ofs, " "};
 
+					copy (source.begin(), source.end(), oi);
+					ofs.close();
+				}
+
+				return filename;
+			}
+
+			vector <Item> import (const string & filename)
+			{
 				vector <Item>
 					vi {10, Item {}};
 				ifstream
 					ifs {filename};
 				istream_iterator <Item>
 					ii {ifs};
+
 				copy (ii, istream_iterator <Item> {}, vi.begin());
 
 				ifs.close();
 
-				for (auto a : vi)
+				return vi;
+			}
+
+			void local_1 ()
+			{
+				for (auto a : import (initial (false)))
 					cout
-						<< a.to_string() << '\n';
+					<< a.to_string() << '\n';
+			}
+
+			void local_2 ()
+			{
+
+
+
 			}
 		}
 
 		void main()
 		{
-			_01::local();
+			_01::local_1();
 		}
 	}
 }
